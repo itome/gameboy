782dab232c1c640e1d277262a6cb21e5ff4852fc
Takeshi Tsukamoto <dev@itome.team>
2023-11-26 16:05:13 +0900

    Cpu instructions

--- src/instructions.rs
+++ src/instructions.rs
@@ -72,11 +72,7 @@ impl Cpu {
         Self: IO8<S>,
     {
         if let Some(v) = self.read8(bus, src) {
-            let (result, curry) = self.regs.a.overflowing_sub(v);
-            self.regs.set_zf(result == 0);
-            self.regs.set_nf(true);
-            self.regs.set_hf((self.regs.a & 0xf) < (v & 0xf));
-            self.regs.set_cf(curry);
+            self.sub_general(v, false);
             self.fetch(bus);
         }
     }
@@ -165,12 +161,7 @@ impl Cpu {
     {
         step!((), {
             0: if let Some(v) = self.read8(bus, src) {
-                let result = (v << 1) | self.regs.cf() as u8;
-                self.regs.set_zf(result == 0);
-                self.regs.set_nf(false);
-                self.regs.set_hf(false);
-                self.regs.set_cf(v & 0x80 > 0);
-                VAL8.store(result, Relaxed);
+                VAL8.store(self.rl_general(v), Relaxed);
                 go!(1);
             },
             1: if self.write8(bus, src, VAL8.load(Relaxed)).is_some() {
@@ -275,15 +266,6 @@ impl Cpu {
         });
     }
 
-    fn cond(&self, cond: Cond) -> bool {
-        match cond {
-            Cond::NZ => !self.regs.zf(),
-            Cond::Z => self.regs.zf(),
-            Cond::NC => !self.regs.cf(),
-            Cond::C => self.regs.cf(),
-        }
-    }
-
     pub fn jr_c(&mut self, bus: &Peripherals, c: Cond) {
         step!((), {
             0: if let Some(v) = self.read8(bus, Imm8) {
@@ -365,5 +347,536 @@ impl Cpu {
                 }
             },
         });
+    }
+
+    pub fn add<S: Copy>(&mut self, bus: &mut Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        if let Some(v) = self.read8(bus, src) {
+            let (result, carry) = self.regs.a.overflowing_add(v);
+            self.regs.set_zf(result == 0);
+            self.regs.set_nf(false);
+            self.regs.set_hf((self.regs.a & 0xf) + (v & 0xf) > 0xf);
+            self.regs.set_cf(carry);
+            self.regs.a = result;
+            self.fetch(bus);
+        }
+    }
+
+    pub fn adc<S: Copy>(&mut self, bus: &mut Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        let cy = self.regs.cf() as u8;
+        if let Some(v) = self.read8(bus, src) {
+            let result = self.regs.a.wrapping_add(v).wrapping_add(cy);
+            self.regs.set_zf(result == 0);
+            self.regs.set_nf(false);
+            self.regs.set_hf((self.regs.a & 0xf) + (v & 0xf) + cy > 0xf);
+            self.regs
+                .set_cf(self.regs.a as u16 + v as u16 + cy as u16 > 0xff);
+            self.regs.a = result;
+            self.fetch(bus);
+        }
+    }
+
+    pub fn sub<S: Copy>(&mut self, bus: &mut Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        if let Some(v) = self.read8(bus, src) {
+            self.regs.a = self.sub_general(v, false);
+            self.fetch(bus);
+        }
+    }
+
+    pub fn sbc<S: Copy>(&mut self, bus: &mut Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        if let Some(v) = self.read8(bus, src) {
+            self.regs.a = self.sub_general(v, self.regs.cf());
+            self.fetch(bus);
+        }
+    }
+
+    pub fn and<S: Copy>(&mut self, bus: &mut Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        if let Some(v) = self.read8(bus, src) {
+            self.regs.a &= v;
+            self.regs.set_zf(self.regs.a == 0);
+            self.regs.set_nf(false);
+            self.regs.set_hf(true);
+            self.regs.set_cf(false);
+            self.fetch(bus);
+        }
+    }
+
+    pub fn or<S: Copy>(&mut self, bus: &mut Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        if let Some(v) = self.read8(bus, src) {
+            self.regs.a |= v;
+            self.regs.set_zf(self.regs.a == 0);
+            self.regs.set_nf(false);
+            self.regs.set_hf(false);
+            self.regs.set_cf(false);
+            self.fetch(bus);
+        }
+    }
+
+    pub fn xor<S: Copy>(&mut self, bus: &mut Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        if let Some(v) = self.read8(bus, src) {
+            self.regs.a ^= v;
+            self.regs.set_zf(self.regs.a == 0);
+            self.regs.set_nf(false);
+            self.regs.set_hf(false);
+            self.regs.set_cf(false);
+            self.fetch(bus);
+        }
+    }
+
+    pub fn rlca(&mut self, bus: &mut Peripherals) {
+        self.regs.a = self.rlc_general(self.regs.a);
+        self.regs.set_zf(false);
+        self.fetch(bus);
+    }
+
+    pub fn rla(&mut self, bus: &mut Peripherals) {
+        self.regs.a = self.rl_general(self.regs.a);
+        self.regs.set_zf(false);
+        self.fetch(bus);
+    }
+
+    pub fn rrca(&mut self, bus: &mut Peripherals) {
+        self.regs.a = self.rrc_general(self.regs.a);
+        self.regs.set_zf(false);
+        self.fetch(bus);
+    }
+
+    pub fn rra(&mut self, bus: &mut Peripherals) {
+        self.regs.a = self.rr_general(self.regs.a);
+        self.regs.set_zf(false);
+        self.fetch(bus);
+    }
+
+    pub fn rlc<S: Copy>(&mut self, bus: &mut Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        step!((), {
+            0: if let Some(v) = self.read8(bus, src) {
+                VAL8.store(self.rlc_general(v), Relaxed);
+                go!(1);
+            },
+            1: if self.write8(bus, src, VAL8.load(Relaxed)).is_some() {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn rrc<S: Copy>(&mut self, bus: &mut Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        step!((), {
+            0: if let Some(v) = self.read8(bus, src) {
+                VAL8.store(self.rrc_general(v), Relaxed);
+                go!(1);
+            },
+            1: if self.write8(bus, src, VAL8.load(Relaxed)).is_some() {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn rr<S: Copy>(&mut self, bus: &mut Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        step!((), {
+            0: if let Some(v) = self.read8(bus, src) {
+                VAL8.store(self.rr_general(v), Relaxed);
+                go!(1);
+            },
+            1: if self.write8(bus, src, VAL8.load(Relaxed)).is_some() {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn sla<S: Copy>(&mut self, bus: &mut Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        step!((), {
+            0: if let Some(v) = self.read8(bus, src) {
+                self.regs.set_zf(v & 0x7f == 0);
+                self.regs.set_nf(false);
+                self.regs.set_hf(false);
+                self.regs.set_cf(v & 0x80 > 0);
+                VAL8.store(v << 1, Relaxed);
+                go!(1);
+            },
+            1: if self.write8(bus, src, VAL8.load(Relaxed)).is_some() {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn sra<S: Copy>(&mut self, bus: &mut Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        step!((), {
+            0: if let Some(v) = self.read8(bus, src) {
+                self.regs.set_zf(v & 0xFE == 0);
+                self.regs.set_nf(false);
+                self.regs.set_hf(false);
+                self.regs.set_cf(v & 1 > 0);
+                VAL8.store((v & 0x80) | (v >> 1), Relaxed);
+                go!(1);
+            },
+            1: if self.write8(bus, src, VAL8.load(Relaxed)).is_some() {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn srl<S: Copy>(&mut self, bus: &mut Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        step!((), {
+            0: if let Some(v) = self.read8(bus, src) {
+                self.regs.set_zf(v & 0xFE == 0);
+                self.regs.set_nf(false);
+                self.regs.set_hf(false);
+                self.regs.set_cf(v & 1 > 0);
+                VAL8.store(v >> 1, Relaxed);
+                go!(1);
+            },
+            1: if self.write8(bus, src, VAL8.load(Relaxed)).is_some() {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn set<S: Copy>(&mut self, bus: &mut Peripherals, bit: usize, src: S)
+    where
+        Self: IO8<S>,
+    {
+        step!((), {
+            0: if let Some(v) = self.read8(bus, src) {
+                VAL8.store(v | (1 << bit), Relaxed);
+                go!(1);
+            },
+            1: if self.write8(bus, src, VAL8.load(Relaxed)).is_some() {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn res<S: Copy>(&mut self, bus: &mut Peripherals, bit: usize, src: S)
+    where
+        Self: IO8<S>,
+    {
+        step!((), {
+            0: if let Some(v) = self.read8(bus, src) {
+                VAL8.store(v & !(1 << bit), Relaxed);
+                go!(1);
+            },
+            1: if self.write8(bus, src, VAL8.load(Relaxed)).is_some() {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn jp(&mut self, bus: &Peripherals) {
+        step!((), {
+            0: if let Some(v) = self.read16(bus, Imm16) {
+                self.regs.pc = v;
+                return go!(1);
+            },
+            1: {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn jp_hl(&mut self, bus: &Peripherals) {
+        self.regs.pc = self.regs.hl();
+        self.fetch(bus);
+    }
+
+    pub fn jp_c(&mut self, bus: &Peripherals, cond: Cond) {
+        step!((), {
+            0: if let Some(v) = self.read16(bus, Imm16) {
+                go!(1);
+                if self.cond(cond) {
+                    self.regs.pc = v;
+                    return;
+                }
+            },
+            1: {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn call_c(&mut self, bus: &mut Peripherals, cond: Cond) {
+        step!((), {
+            0: if let Some(v) = self.read16(bus, Imm16) {
+                VAL16.store(v, Relaxed);
+                if self.cond(cond) {
+                    go!(1);
+                } else {
+                    self.fetch(bus);
+                }
+            },
+            1: if self.push16(bus, self.regs.pc).is_some() {
+                self.regs.pc = VAL16.load(Relaxed);
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn ret_c(&mut self, bus: &Peripherals, cond: Cond) {
+        step!((), {
+            0: return go!(1),
+            1: go!(if self.cond(cond) { 2 } else { 3 }),
+            2: if let Some(v) = self.pop16(bus) {
+                self.regs.pc = v;
+                return go!(3);
+            },
+            3: {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn rst(&mut self, bus: &mut Peripherals, addr: u8) {
+        if self.push16(bus, self.regs.pc).is_some() {
+            self.regs.pc = addr as u16;
+            self.fetch(bus);
+        }
+    }
+
+    pub fn stop(&mut self, _: &Peripherals) {
+        panic!("STOP");
+    }
+
+    pub fn swap<S: Copy>(&mut self, bus: &mut Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        step!((), {
+            0: if let Some(v) = self.read8(bus, src) {
+                self.regs.set_zf(v == 0);
+                self.regs.set_nf(false);
+                self.regs.set_hf(false);
+                self.regs.set_cf(false);
+                VAL8.store((v << 4) | (v >> 4), Relaxed);
+                go!(1);
+            },
+            1: if self.write8(bus, src, VAL8.load(Relaxed)).is_some() {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn ccf(&mut self, bus: &Peripherals) {
+        self.regs.set_nf(false);
+        self.regs.set_hf(false);
+        self.regs.set_cf(!self.regs.cf());
+        self.fetch(bus);
+    }
+
+    pub fn scf(&mut self, bus: &Peripherals) {
+        self.regs.set_nf(false);
+        self.regs.set_hf(false);
+        self.regs.set_cf(true);
+        self.fetch(bus);
+    }
+
+    pub fn daa(&mut self, bus: &Peripherals) {
+        let mut cf = false;
+        if !self.regs.nf() {
+            if self.regs.cf() || self.regs.a > 0x99 {
+                cf = true;
+                self.regs.a = self.regs.a.wrapping_add(0x60);
+            }
+            if self.regs.hf() || self.regs.a & 0x0f > 0x09 {
+                self.regs.a = self.regs.a.wrapping_add(0x06);
+            }
+        } else {
+            if self.regs.cf() {
+                cf = true;
+                if self.regs.hf() {
+                    self.regs.a = self.regs.a.wrapping_add(0x9A);
+                } else {
+                    self.regs.a = self.regs.a.wrapping_add(0xA0);
+                }
+            } else if self.regs.hf() {
+                self.regs.a = self.regs.a.wrapping_add(0xFA);
+            }
+        }
+        self.regs.set_zf(self.regs.a == 0);
+        self.regs.set_hf(false);
+        self.regs.set_cf(cf);
+        self.fetch(bus);
+    }
+
+    pub fn cpl(&mut self, bus: &Peripherals) {
+        self.regs.a = !self.regs.a;
+        self.regs.set_nf(true);
+        self.regs.set_hf(true);
+        self.fetch(bus);
+    }
+
+    pub fn ld_sp_hl(&mut self, bus: &Peripherals) {
+        step!((), {
+            0: {
+                self.regs.sp = self.regs.hl();
+                return go!(1);
+            },
+            1: {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn ld_hl_sp_e(&mut self, bus: &Peripherals) {
+        step!((), {
+            0: if let Some(v) = self.read8(bus, Imm8) {
+                let val = v as i8 as u16;
+                self.regs.set_zf(false);
+                self.regs.set_nf(false);
+                self.regs.set_hf((self.regs.sp & 0xF) + (val & 0xF) > 0xF);
+                self.regs.set_cf((self.regs.sp & 0xFF) + (val & 0xFF) > 0xFF);
+                self.regs.write_hl(self.regs.sp.wrapping_add(val));
+                return go!(1);
+            },
+            1: {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn add_hl_reg16(&mut self, bus: &Peripherals, src: Reg16) {
+        step!((), {
+            0: {
+                let val = self.read16(bus, src).unwrap();
+                let (result, carry) = self.regs.hl().overflowing_add(val);
+                self.regs.set_nf(false);
+                self.regs.set_hf((self.regs.hl() & 0xFFF) + (val & 0xFFF) > 0x0FFF);
+                self.regs.set_cf(carry);
+                self.regs.write_hl(result);
+                return go!(1);
+            },
+            1: {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn add_sp_e(&mut self, bus: &Peripherals) {
+        step!((), {
+            0: if let Some(v) = self.read8(bus, Imm8) {
+                let val = v as i8 as u16;
+                self.regs.set_zf(false);
+                self.regs.set_nf(false);
+                self.regs.set_hf((self.regs.sp & 0xF) + (val & 0xF) > 0xF);
+                self.regs.set_cf((self.regs.sp & 0xFF) + (val & 0xFF) > 0xFF);
+                self.regs.sp = self.regs.sp.wrapping_add(val);
+                return go!(1);
+            },
+            1: return go!(2),
+            2: {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn undefined(&mut self, _: &Peripherals) {
+        panic!("Undefined opcode {:02x}", self.ctx.opcode);
+    }
+
+    fn cond(&self, cond: Cond) -> bool {
+        match cond {
+            Cond::NZ => !self.regs.zf(),
+            Cond::Z => self.regs.zf(),
+            Cond::NC => !self.regs.cf(),
+            Cond::C => self.regs.cf(),
+        }
+    }
+
+    fn sub_general(&mut self, val: u8, carry: bool) -> u8 {
+        let cy = carry as u8;
+        let result = self.regs.a.wrapping_sub(val).wrapping_sub(cy);
+        self.regs.set_zf(result == 0);
+        self.regs.set_nf(true);
+        self.regs.set_hf((self.regs.a & 0xf) < (val & 0xf) + cy);
+        self.regs
+            .set_cf((self.regs.a as u16) < (val as u16) + (cy as u16));
+        result
+    }
+
+    fn rlc_general(&mut self, val: u8) -> u8 {
+        self.regs.set_zf(val == 0);
+        self.regs.set_nf(false);
+        self.regs.set_hf(false);
+        self.regs.set_cf(val & 0x80 > 0);
+        (val << 1) | (val >> 7)
+    }
+
+    fn rl_general(&mut self, val: u8) -> u8 {
+        let new_val = (val << 1) | self.regs.cf() as u8;
+        self.regs.set_zf(new_val == 0);
+        self.regs.set_nf(false);
+        self.regs.set_hf(false);
+        self.regs.set_cf(val & 0x80 > 0);
+        new_val
+    }
+
+    fn rrc_general(&mut self, val: u8) -> u8 {
+        self.regs.set_zf(val == 0);
+        self.regs.set_nf(false);
+        self.regs.set_hf(false);
+        self.regs.set_cf(val & 1 > 0);
+        (val << 7) | (val >> 1)
+    }
+
+    fn rr_general(&mut self, val: u8) -> u8 {
+        let new_val = ((self.regs.cf() as u8) << 7) | (val >> 1);
+        self.regs.set_zf(new_val == 0);
+        self.regs.set_nf(false);
+        self.regs.set_hf(false);
+        self.regs.set_cf(val & 1 > 0);
+        new_val
     }
 }
